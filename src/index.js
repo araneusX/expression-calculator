3cb2aedf443784a1b7a6639c4b33cec1657b7643
araneusX <araneusX@gmail.com>
2020-03-04 16:48:49 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,93 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  const action = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b
+  }
+
+  const priority = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 3
+  }
+
+  function stringToRNP (expr) {
+    
+    const exprRNP = [];
+    const operators = [];
+    let numberStr = '';
+    
+    for (let i = 0; i < expr.length; i++){
+      if (/\D/.test(expr[i])) {
+
+        if (numberStr.length > 0) {
+          exprRNP.push(Number(numberStr));
+          numberStr = '';
+        }
+
+        if (expr[i] === '(') {
+          operators.push(expr[i]);
+        }else if (expr[i] === ')') {
+          while (operators[operators.length - 1] !== '(') {
+            if (operators.length === 0) {
+              throw "ExpressionError: Brackets must be paired";
+            }
+            exprRNP.push(operators.pop());
+          }
+          operators.pop();
+        }else if (expr[i] in action) {
+          while (operators.length > 0 && priority[expr[i]] <= priority[operators[operators.length - 1]]) {
+            exprRNP.push(operators.pop());
+          }
+          operators.push(expr[i]);
+        }
+      }else{
+        numberStr += expr[i];
+      }
+    }
+
+    if (numberStr.length > 0) {
+      exprRNP.push(Number(numberStr));
+    }
+    
+    operators.reverse().forEach(item => {
+      if (item in action) {
+        exprRNP.push(item);
+      }else {
+        throw "ExpressionError: Brackets must be paired";
+      }
+    });
+
+    return exprRNP;
+  }
+
+  function calculateRNPExpr (exprRNP) {
+    const stack = [];
+    
+    exprRNP.forEach(item => {
+      if (item in action) {
+        if (item === '/' && stack[stack.length - 1] === 0) {
+          throw "TypeError: Division by zero.";
+        }
+        
+        let b = stack.pop();
+        let a = stack.pop();
+        stack.push(action[item](a, b));
+
+      }else{
+        stack.push(item);
+      }
+
+    })
+
+    return stack[0];
+  }
+
+  return (calculateRNPExpr(stringToRNP(expr)));
 }
 
 module.exports = {
